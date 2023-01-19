2b9314b4c76fc5137113ba41157bee4c0cf3ea1b
Patrick Roza <contact@patrickroza.com>
2023-01-19 16:14:12 +0100

    Restructure

--- starter-vite-tsc/vite.config.ts
+++ starter-vite-tsc/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import { effectPlugin } from "@effect/vite-plugin-react";
+// import { tsPlugin as effectPlugin } from "@effect-app/compiler/vitePlugin";
+
 
 export default defineConfig({
   plugins: [
