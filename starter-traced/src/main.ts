29fa78e941580bfb2a1ecb988941e866fc8eecc2
Patrick Roza <contact@patrickroza.com>
2023-01-20 08:37:06 +0100

    Convert starter-traced to tsplus with effect-app packages

---
+++ starter-traced/src/main.ts
@@ -0,0 +1,25 @@
+import "@effect-app/prelude/_global"
+
+export interface Name {
+  getName: Effect<never, never, string>;
+}
+
+export const Name: Tag<Name> = Tag<Name>();
+
+export const program: Effect<Name, never, void> = Effect.gen(function* ($) {
+  const { getName } = yield* $(Effect.service(Name));
+
+  yield* $(Effect.log(`Hello ${yield* $(getName)}`));
+  yield* $(Effect.die("Error"));
+});
+
+export const NameLive: Layer<never, never, Name> = Layer.effect(Name)(
+  Effect.sync(() => ({
+    getName: Effect.succeed("Mike"),
+  }))
+);
+
+  program
+  .provideLayer(NameLive)
+  .tapErrorCause(_ => Effect.logErrorCauseMessage("Error", _))
+  .unsafeFork
